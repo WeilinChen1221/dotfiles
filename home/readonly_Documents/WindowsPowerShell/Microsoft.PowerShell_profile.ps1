cd105f0f1aefcd1a5d8d59a89349b612f3a63e4a
Weylin Chen <chenweilin1221@gmail.com>
2026-08-22 16:50:54 +0800

    windows: update PowerShell config

--- home/readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,8 @@
 $env:HTTP_PROXY="http://127.0.0.1:10808"
 $env:HTTPS_PROXY="http://127.0.0.1:10808"
 
+Set-Alias -Name notepad -Value notepad3
+
+# uv tool: beet
 $env:VISUAL = 'notepad'
 $env:EDITOR = 'notepad'
