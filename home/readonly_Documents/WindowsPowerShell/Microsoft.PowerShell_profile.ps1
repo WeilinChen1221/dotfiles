2b94b60c79c530765802c8ad9aeb87a087194be4
Weylin Chen <chenweilin1221@gmail.com>
2026-09-16 10:20:01 +0800

    windows: update PowerShell config

--- home/readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,23 @@
 $env:HTTP_PROXY="http://127.0.0.1:10808"
 $env:HTTPS_PROXY="http://127.0.0.1:10808"
 
+# Replace orginal Notepad with Notepad3
 Set-Alias -Name notepad -Value notepad3
-
 # uv tool: beet
 $env:VISUAL = 'notepad'
 $env:EDITOR = 'notepad'
+
+# Fix Chinese character display
+[Console]::InputEncoding  = [System.Text.UTF8Encoding]::new()
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+$OutputEncoding = [System.Text.UTF8Encoding]::new()
+
+# Handle paths that contain escape characters
+function cdl {
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Path
+    )
+
+    Set-Location -LiteralPath $Path
+}
